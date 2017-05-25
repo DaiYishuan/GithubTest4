a927499d3a5b0cc64b0d46975b30172d33dd8501
yishuan <edward0487@gmail.com>
2017-05-25 15:56:15 +0800

    <3

--- app/src/main/java/com/s10576019/cyel/githubtest4/Page亦軒.java
+++ app/src/main/java/com/s10576019/cyel/githubtest4/Page亦軒.java
@@ -1,6 +1,9 @@
 package com.s10576019.cyel.githubtest4;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by Jim on 2017/5/25.
@@ -8,4 +11,15 @@ import android.support.v7.app.AppCompatActivity;
 
 //TODO 完成Page亦軒
 public class Page亦軒 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page_yishuan);
+    }
+
+    public void clickButtonStart(View view) {
+        Intent intent = new Intent(this, Page劉濬.class);
+        startActivity(intent);
+    }
 }
